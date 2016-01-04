407131e8c870da913d12a12c583ef0a1b9acf40e
sebabouche <s.nicolaidis@me.com>
2016-01-04 23:38:34 +0100

    Updating config, initializers and ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,16 @@
 class ApplicationController < ActionController::Base
+  include Trailblazer::Operation::Controller
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  include Trailblazer::Operation::Controller
-  require 'trailblazer/operation/controller/active_record'
-  include Trailblazer::Operation::Controller::ActiveRecord # named instance variables.
-
   def tyrant
-    Tyrant::Session.new(request.env['warden'])
+    Tyrant::Session::new(request.env["warden"])
   end
-
   helper_method :tyrant
 
   def process_params!(params)
     params.merge!(current_user: tyrant.current_user)
-  end  
-  
+  end
 end
+
